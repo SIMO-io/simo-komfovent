87f3ff709eddd57f91e42fc9b6467e30dadefeaa
simanas <simanas.venckauskas@gmail.com>
2024-11-19 09:53:23 +0200

    Better tasks and be_or_not_to_be function

--- src/simo_komfovent/tasks.py
+++ src/simo_komfovent/tasks.py
@@ -1,7 +1,10 @@
 import random
+import time
 from celeryc import celery_app
 from django.utils import timezone
+from simo.core.middleware import introduce_instance
 from simo.core.models import Instance, Component
+from simo.generic.scripting.helpers import be_or_not_to_be
 from .controllers import RecuperatorFilterContamination
 
 
@@ -11,14 +14,11 @@ def notify_on_clogged_filters():
     from simo.notifications.utils import notify_users
     for instance in Instance.objects.filter(is_active=True):
         timezone.activate(instance.timezone)
+        introduce_instance(instance)
         hour = timezone.localtime().hour
         if hour < 7:
             continue
         if hour > 21:
-            continue
-        if hour % 1 == 0:
-            continue
-        if not random.choice([True, False, False, False, False]):
             continue
 
         for comp in Component.objects.filter(
@@ -26,6 +26,11 @@ def notify_on_clogged_filters():
             controller_uid=RecuperatorFilterContamination.uid,
         ):
             if comp.value < comp.config.get('notify_on_level'):
+                continue
+
+            last_warning = comp.meta.get('last_warning', 0)
+            notify = be_or_not_to_be(12 * 60 * 60, 72 * 60 * 60, last_warning)
+            if not notify:
                 continue
 
             iusers = comp.zone.instance.instance_users.filter(
@@ -37,6 +42,8 @@ def notify_on_clogged_filters():
                     f"Filters are {comp.value}% clogged!",
                     component=comp, instance_users=iusers
                 )
+                comp.meta['last_warning'] = time.time()
+                comp.save()
 
 
 @celery_app.on_after_finalize.connect
